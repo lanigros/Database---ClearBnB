918709ee7dde378384bde2bfbe710635834d5a75
Alex <alexanderekdahl@me.com>
2021-09-14 15:40:47 +0200

    added xml for workbench

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -2,14 +2,14 @@ import express.Express;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Express app = new Express();
+  public static void main(String[] args) {
+    Express app = new Express();
 
-        app.get("/", (req, res) -> {
-            res.send("Hello World");
-        });
+    app.get("/", (req, res) -> {
+      res.send("Hello World");
+    });
 
-        app.listen(4000); // Start server on port 4000
-    }
+    app.listen(4000); // Start server on port 4000
+  }
 
 }
