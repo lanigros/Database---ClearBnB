3f4047979e1fe9d117c53b6be9fa9cb0e45692fd
Alex <alexanderekdahl@me.com>
2021-09-14 12:27:14 +0200

    init

---
+++ server/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import express.Express;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Express app = new Express();
+
+        app.get("/", (req, res) -> {
+            res.send("Hello World");
+        });
+
+        app.listen(4000); // Start server on port 4000
+    }
+
+}
