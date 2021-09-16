d938114c60f4bb8de25af84f60001b6bd1a277d0
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-16 13:16:26 +0200

    Created the model Home
    Created the Home Model

---
+++ server/src/main/java/model/Home.java
@@ -0,0 +1,103 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="home")
+public class Home {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "host_id", nullable = false)
+    @ManyToOne //MANY homes to ONE host
+    private int hostId;
+
+    @Column(name = "address_id", nullable = false)
+    @OneToOne //ONE address to ONE home
+    private int addressId;
+
+    @Column(name = "img_url", nullable = false)
+    private String imgUrl;
+
+    @Column(name = "price_per_night", nullable = false)
+    private int pricePerNight;
+
+    @Column(name = "start_date", nullable = false)
+    private String startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private String endDate;
+
+    @Column(name = "updated_date", nullable = false)
+    private String updatedDate;
+
+    @Column(name = "created_date", nullable = false)
+    private String createdDate;
+
+    public int getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(int hostId) {
+        this.hostId = hostId;
+    }
+
+    public int getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public int getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(int pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(String updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+}
