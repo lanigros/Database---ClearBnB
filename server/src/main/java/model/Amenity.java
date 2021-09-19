6e3eceefd38ce5504a815dffe47e3f20c312f3a2
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-19 14:28:18 +0200

    Added Getters and Setters
    As stated above, added getters and setters to the Amenity class. I also added the toString method.

--- server/src/main/java/model/Amenity.java
+++ server/src/main/java/model/Amenity.java
@@ -20,4 +20,42 @@ public class Amenity {
     @Enumerated(EnumType.STRING)
     private String amenity;
 
+    public Amenity(int id, Home home, String amenity) {
+        this.id = id;
+        this.home = home;
+        this.amenity = amenity;
+    }
+
+    public Amenity() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Home getHome() {
+        return home;
+    }
+
+    public void setHome(Home home) {
+        this.home = home;
+    }
+
+    public String getAmenity() {
+        return amenity;
+    }
+
+    public void setAmenity(String amenity) {
+        this.amenity = amenity;
+    }
+
+    @Override
+    public String toString() {
+        return "Amenity{" + "id=" + id + ", home=" + home + ", amenity='" + amenity + '\'' + '}';
+    }
+
 }
