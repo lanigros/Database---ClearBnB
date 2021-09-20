323c0de6f78c5491d936633420fa8f7a98c43214
Alex <alexanderekdahl@me.com>
2021-09-20 18:30:17 +0200

    implemented DTOs in login and register

--- server/src/main/java/model/User.java
+++ server/src/main/java/model/User.java
@@ -1,9 +1,12 @@
 package model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
+import datatransforobject.UserCoreDTO;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -18,6 +21,7 @@ import javax.persistence.Table;
 public class User {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Integer id;
   @Column(name = "first_name")
   private String firstName;
@@ -104,5 +108,9 @@ public class User {
 
   public void setRenterProfile(Renter renterProfile) {
     this.renterProfile = renterProfile;
+  }
+
+  public UserCoreDTO convertToUserCoreDTO() {
+    return new UserCoreDTO(this.id, this.firstName, this.lastName, this.email, this.password);
   }
 }
