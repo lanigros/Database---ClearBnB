522f786fd9b59669810a897c521b1c9421a3efad
Alex <alexanderekdahl@me.com>
2021-09-20 11:23:02 +0200

    Added and implemented interface

--- server/src/main/java/repository/AmenityRepository.java
+++ server/src/main/java/repository/AmenityRepository.java
@@ -1,10 +1,24 @@
 package repository;
 
+import java.util.List;
+import java.util.Optional;
 import javax.persistence.EntityManager;
+import model.Amenity;
+import repositoryinterface.AmenityRepositoryInterface;
 
-public class AmenityRepository {
+public class AmenityRepository implements AmenityRepositoryInterface {
 
     EntityManager entityManager;
+
+    @Override
+    public Optional<Amenity> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Amenity> findAll() {
+        return null;
+    }
 
     public AmenityRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
