755101596b5e60660b15f115fba05149154ccbd4
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-09-20 08:23:12 +0200

    Corrected image to home_image as table name

--- server/src/main/java/model/HomeImage.java
+++ server/src/main/java/model/HomeImage.java
@@ -11,7 +11,7 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "image")
+@Table(name = "home_image")
 @NamedQueries({
     @NamedQuery(name = "HomeImage.findById", query = "SELECT h FROM HomeImage h WHERE h.id = :id"),
     @NamedQuery(name = "HomeImage.findAll", query = "SELECT h FROM HomeImage h")
