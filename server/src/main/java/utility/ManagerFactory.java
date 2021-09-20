b07b6e657aa58bf6368fb992e11e3735850a5729
Alex <alexanderekdahl@me.com>
2021-09-20 13:45:44 +0200

    renamed

--- server/src/main/java/utility/ManagerFactory.java
+++ server/src/main/java/utility/ManagerFactory.java
@@ -3,17 +3,18 @@ package utility;
 import io.github.cdimascio.dotenv.Dotenv;
 import java.util.HashMap;
 import java.util.Map;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class EntityManagerFactory {
+public class ManagerFactory {
 
-  private EntityManagerFactory getEntityManagerFactory() {
-    return Persistence.createEntityManagerFactory(getPersistenceUnitName(), getProperties());
+  public static EntityManagerFactory getEntityManagerFactory(String entity) {
+    return Persistence.createEntityManagerFactory(entity, getProperties());
   }
 
-  private Map getProperties() {
+  private static Map<String, String> getProperties() {
     Dotenv dotenv = Dotenv.load();
-    Map result = new HashMap();
+    Map<String, String> result = new HashMap<>();
 
     // Read the properties from a file instead of hard-coding it here.
     // Or pass the password in from the command-line.
