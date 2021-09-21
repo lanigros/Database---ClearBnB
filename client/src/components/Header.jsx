4c87ebea00e419cacdc951e6b49e2d78fc9ba649
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-09-21 12:30:06 +0200

    Added login and logout and username to header
    - Logging out now redirects to homepage
    
    - Logging in now redirects to user profile
    
    - Being logged in now displays user's name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Link } from 'react-router-dom'
 import HeaderStyle from './Header.styles'
+import { Context } from '../store/Store'
 
 const Header = () => {
+  const [state, dispatch] = useContext(Context)
+
+  const logout = () => {
+    dispatch({ type: 'SET_USER', payload: ''})
+  }
+
   return (
     <HeaderStyle>
       <header>
@@ -11,6 +18,10 @@ const Header = () => {
           <Link to={'/create-user'}>Create user</Link>
           <Link to={'/users'}>See users</Link>
           <Link to={'/homes'}>See Homes</Link>
+        </nav>
+        <nav>
+          {state.currentUser ? <Link to={'/'} onClick={logout}>Logout</Link> : <Link to={'/login'}>Login</Link>}
+          {state.currentUser && <Link to={'/my-profile'}> {state.currentUser} </Link>}
         </nav>
       </header>
     </HeaderStyle>
