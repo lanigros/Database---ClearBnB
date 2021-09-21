165084727228ca3bc240443a1984fbb752eac080
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-21 09:00:30 +0200

    Made the converter work.
    After several tries, we made the converter work.
    - Created the AmenityConverter class
    -  Added the method "convertToDatabaseColumn" which is kinda self explanatory. It takes the enum and converts it so it works with the database column.
    - Added the case "convertToEntityAttribute" that takes the enum from the database and converts it so it prints out the amenities that are set on the specific house
    
    Co-Authored-By: Alexander Ekdahl <77007088+AlexEkdahl@users.noreply.github.com>
    Co-Authored-By: Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>

--- server/src/main/java/utility/AmenityConverter.java
+++ server/src/main/java/utility/AmenityConverter.java
@@ -2,50 +2,30 @@ package utility;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
-import model.Amenity;
-import model.Amenity.AMENITY_ENUM;
-
-public class AmenityConverter implements AttributeConverter<Amenity, String> {
-
-    private static final String SEPARATOR = "\\|";
-
-    //Convert amenity object to string
-    //with format WIFI|KITCHEN|PARKING|BATH
-    @Override
-    public String convertToDatabaseColumn(Amenity amenity) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(AMENITY_ENUM.WIFI).append(SEPARATOR).append(AMENITY_ENUM.KITCHEN)
-                     .append(SEPARATOR).append(AMENITY_ENUM.PARKING).append(SEPARATOR)
-                     .append(AMENITY_ENUM.BATH);
-        return stringBuilder.toString();
-    }
-    //Convert a String with format red|green|blue|alpha to an Amenity object
-
-    @Override
-    public Amenity convertToEntityAttribute(String amenity_enum) {
-        String[] amenity = amenity_enum.split(SEPARATOR);
-        return new Amenity(amenity);
-    }
-
-}
+import model.Amenity.AmenityEnum;
 
 @Converter(autoApply = true)
-public class NameAttributeConveter implements AttributeConverter<Amenity, String> {
+public class AmenityConverter implements AttributeConverter<AmenityEnum, String> {
 
     @Override
-    public String convertToDatabaseColumn(Amenity attribute) {
-        String amenities = attribute.getAmenity() == null ? "No amenities on this home" : attribute.getAmenity();
-        return amenities;
+    public String convertToDatabaseColumn(AmenityEnum amenities) {
+        return amenities.toString();
     }
 
     @Override
-    public Amenity convertToEntityAttribute(String dbData) {
-        if (dbData != null && dbData.split(" ").length > 0) {
-            Amenity amenity = new Amenity();
-            amenity.setAmenity(dbData.split("").length > 0);
-            return amenity;
+    public AmenityEnum convertToEntityAttribute(String s) {
+        switch (s) {
+            case "WIFI":
+                return AmenityEnum.WIFI;
+            case "KITCHEN":
+                return AmenityEnum.KITCHEN;
+            case "PARKING":
+                return AmenityEnum.PARKING;
+            case "BATH":
+                return AmenityEnum.BATH;
+            default:
+                return null;
         }
-        return null;
     }
 
 }
