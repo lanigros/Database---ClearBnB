bbcb2542b2359eab128330e013df663a39af3f67
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-22 21:48:14 +0200

    Removed HomeHistoryService import and instance

--- server/src/main/java/routes/HomeHistoryRoutes.java
+++ server/src/main/java/routes/HomeHistoryRoutes.java
@@ -6,12 +6,12 @@ import java.util.List;
 import java.util.Optional;
 import model.Home;
 import model.HomeHistoryLog;
-import service.HomeHistoryService;
+
 import service.HomeService;
 
 public class HomeHistoryRoutes {
     private final Express app;
-    private final HomeHistoryService homeHistoryService;
+
     private final HomeService homeService;
     private final Home home;
     private final HomeHistoryLog homeHistoryLog;
@@ -20,7 +20,7 @@ public class HomeHistoryRoutes {
 
     public HomeHistoryRoutes(Express app){
         this.app = app;
-        this.homeHistoryService= new HomeHistoryService();
+
         this.homeService = new HomeService();
         this.home = new Home();
         this.homeHistoryLog = new HomeHistoryLog();
