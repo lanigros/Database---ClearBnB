7885e74763de46edf6cc57bbb419310f8cf78a2a
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-22 21:45:38 +0200

    Added HomeHistoryDTO to HomeService

--- server/src/main/java/service/HomeService.java
+++ server/src/main/java/service/HomeService.java
@@ -1,11 +1,15 @@
 package service;
 
 import datatransforobject.HomeCoreDTO;
+import datatransforobject.HomeHistoryDTO;
+import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import mapper.HomeMapper;
 import model.Home;
+import model.HomeHistoryLog;
+import repository.HomeHistoryLogRepository;
 import repository.HomeRepository;
 import utility.ManagerFactory;
 
@@ -15,6 +19,8 @@ public class HomeService {
       "Home");
   private final EntityManager entityManager = entityManagerFactory.createEntityManager();
   private final HomeRepository homeRepository = new HomeRepository(entityManager);
+  private final HomeHistoryLogRepository homeHistoryLogRepository =
+          new HomeHistoryLogRepository(entityManager);
 
   public Optional<HomeCoreDTO> getById(String id) {
 
@@ -27,5 +33,22 @@ public class HomeService {
     return Optional.of(HomeMapper.convertToCore(homeDO.get()));
 
   }
+  public Optional<HomeHistoryDTO> getId(String id) {
+
+        Optional<HomeHistoryLog> homeHistoryDO = homeHistoryLogRepository.findById(id);
+        if (homeHistoryDO.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(HomeMapper.convertToCore(homeHistoryDO.get()));
+
+    }
+
+    public List<HomeHistoryLog> getByHomeId(String homeId) {
+        List<HomeHistoryLog> homeHistoryHomeId =
+                homeHistoryLogRepository.findByHomeId(homeId);
+        return homeHistoryHomeId;
+
+    }
 }
 
