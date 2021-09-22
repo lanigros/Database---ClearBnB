55e433e4f5e7af8f91a55974573781393ff1e644
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-09-22 02:02:14 +0200

    Added api routes for login and logout calls to userAPI

--- client/src/api/userAPI.js
+++ client/src/api/userAPI.js
@@ -17,3 +17,34 @@ export const fetchUserReviews = async (userId) => {
   const reviews = await (await fetch(`/rest/users/${userId}/reviews`)).json()
   return reviews
 }
+
+export const getActiveUser = async () => {
+  const response = await fetch(`/api/whoami`)
+  if (!response.ok) return null
+  const userSession = await response.json()
+  const user = JSON.parse(localStorage.getItem('current-user'))
+  if (userSession.id === user.id && userSession.id != null) {
+    return userSession
+  }
+  return null
+}
+
+export const loginUser = async (userLogin) => {
+  const response = await fetch(`/api/login`, {
+    body: JSON.stringify(userLogin),
+    method: 'POST'
+  })
+  if (!response.ok) return null
+  const user = await response.json()
+  localStorage.setItem('current-user', JSON.stringify(user))
+  return user
+}
+
+export const logoutUser = async (userLogout) => {
+  const response = await fetch(`/api/logout`, {
+        body: localStorage.getItem('current-user'),
+        method: 'DELETE'
+      })
+      if (!response.ok) return null
+      return 'OK'
+}
