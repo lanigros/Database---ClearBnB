3ed303c194452f707d4d4e617cc003255832b0de
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-23 10:29:42 +0200

    A few changes on HomeMapper
    - Auto correction on the indents (Dont know why it did this now)
    -  added dto.setAmenities(historyLog.getAmenities()); on the HistoryLogDTO

--- server/src/main/java/mapper/HomeMapper.java
+++ server/src/main/java/mapper/HomeMapper.java
@@ -1,6 +1,5 @@
 package mapper;
 
-
 import datatransforobject.HomeCoreDTO;
 import datatransforobject.HomeHistoryDTO;
 import model.Home;
@@ -8,24 +7,22 @@ import model.HomeHistoryLog;
 
 public class HomeMapper {
 
-  public static HomeCoreDTO convertToCore(Home home) {
-    System.out.println(home.getHost());
-    HomeCoreDTO dto = new HomeCoreDTO();
-    dto.setId(home.getId());
-    dto.setAddress(home.getAddress());
-    dto.setHost(HostMapper.convertToHostBasic(home.getHost()));
-    dto.setPricePerNight(home.getPricePerNight());
-    dto.setImages(home.getImages());
-    dto.setStartDate(home.getStartDate());
-    dto.setEndDate(home.getEndDate());
-    dto.setCreatedDate(home.getCreatedDate());
-    dto.setAmenities(home.getAmenities());
-    return dto;
-
-  }
+    public static HomeCoreDTO convertToCore(Home home) {
+        System.out.println(home.getHost());
+        HomeCoreDTO dto = new HomeCoreDTO();
+        dto.setId(home.getId());
+        dto.setAddress(home.getAddress());
+        dto.setHost(HostMapper.convertToHostBasic(home.getHost()));
+        dto.setPricePerNight(home.getPricePerNight());
+        dto.setImages(home.getImages());
+        dto.setStartDate(home.getStartDate());
+        dto.setEndDate(home.getEndDate());
+        dto.setCreatedDate(home.getCreatedDate());
+        dto.setAmenities(home.getAmenities());
+        return dto;
+    }
 
     public static HomeHistoryDTO convertToCore(HomeHistoryLog historyLog) {
-
         HomeHistoryDTO dto = new HomeHistoryDTO();
         dto.setId(historyLog.getId());
         dto.setAddress(historyLog.getHome().getAddress());
@@ -34,9 +31,8 @@ public class HomeMapper {
         dto.setStartDate(historyLog.getStartDate());
         dto.setEndDate(historyLog.getEndDate());
         dto.setCreatedDate(historyLog.getCreatedDate());
-
+        dto.setAmenities(historyLog.getAmenities());
         return dto;
-
     }
 
 }
