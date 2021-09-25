3174ba7ed6fa30f7bcd28c095063c6146407314c
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-25 21:55:09 +0200

    Updated ReviewMapper
    - Edited convertToBasicDTO, removed ID and Created, we don't need them in the DTO
    - Added convertToReview with 4 arguments.

--- server/src/main/java/mapper/ReviewMapper.java
+++ server/src/main/java/mapper/ReviewMapper.java
@@ -3,6 +3,8 @@ package mapper;
 import datatransforobject.ReviewBasicDTO;
 import java.util.ArrayList;
 import java.util.List;
+import model.BookingDetail;
+import model.Host;
 import model.Review;
 
 public class ReviewMapper {
@@ -10,16 +12,24 @@ public class ReviewMapper {
   public static List<ReviewBasicDTO> convertToBasicDTO(List<Review> reviews) {
     List<ReviewBasicDTO> list = new ArrayList<>();
     reviews.forEach(review -> {
-    ReviewBasicDTO dto = new ReviewBasicDTO();
-    dto.setId(review.getId());
-    dto.setComment(review.getComment());
-    dto.setRating(review.getRating());
-    dto.setCreated(review.getCreated());
-    dto.setCreatorId(review.getCreatorId());
-    list.add(dto);
+      ReviewBasicDTO dto = new ReviewBasicDTO();
+      dto.setComment(review.getComment());
+      dto.setRating(review.getRating());
+      dto.setCreatorId(review.getCreatorId());
+      list.add(dto);
     });
-
     return list;
+  }
+
+  public static Review convertToReview(ReviewBasicDTO dto, int userID, BookingDetail bookingDetail,
+      Host host) {
+    Review review = new Review();
+    review.setComment(dto.getComment());
+    review.setRating(dto.getRating());
+    review.setBookingDetail(bookingDetail);
+    review.setCreatorId(userID);
+    review.setHost(host);
+    return review;
   }
 
 }
