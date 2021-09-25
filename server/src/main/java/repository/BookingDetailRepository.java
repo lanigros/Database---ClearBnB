f32f9470f996c2b967b25324487aed09df01e58e
Tobias Gonzalez <tobias.gonzalez.skola@gmail.com>
2021-09-25 21:57:54 +0200

    Update BookingDetailRepository.java
    Changed the argument from String to Int

--- server/src/main/java/repository/BookingDetailRepository.java
+++ server/src/main/java/repository/BookingDetailRepository.java
@@ -4,24 +4,22 @@ import java.util.List;
 import java.util.Optional;
 import javax.persistence.EntityManager;
 import model.BookingDetail;
-import model.Home;
 import repositoryinterface.BookingDetailRepositoryInterface;
-import repositoryinterface.HomeRepositoryInterface;
 
 public class BookingDetailRepository implements BookingDetailRepositoryInterface {
 
   private final EntityManager entityManager;
-
 
   public BookingDetailRepository(EntityManager entityManager) {
     this.entityManager = entityManager;
   }
 
   @Override
-  public Optional<BookingDetail> findById(String id) {
+  public Optional<BookingDetail> findById(int id) {
     BookingDetail bookingDetail = entityManager.find(BookingDetail.class, id);
     return bookingDetail != null ? Optional.of(bookingDetail) : Optional.empty();
   }
+
   @Override
   public List<BookingDetail> findAll() {
     return entityManager.createQuery("from BookingDetail", BookingDetail.class).getResultList();
@@ -39,4 +37,5 @@ public class BookingDetailRepository implements BookingDetailRepositoryInterface
       return Optional.empty();
     }
   }
+
 }
