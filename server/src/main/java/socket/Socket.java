f704fbf77e3231a5d2fa94104e1754317d610851
Alex <alexanderekdahl@me.com>
2021-09-26 12:06:44 +0200

    Send msg to mutiple channels

--- server/src/main/java/socket/Socket.java
+++ server/src/main/java/socket/Socket.java
@@ -1,22 +1,43 @@
 package socket;
 
 import express.Express;
+import io.javalin.websocket.WsContext;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Socket {
 
+  private static Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
+  private static int nextUserNumber = 1;
+
   private final Express app;
 
-  public Socket(Express app){
+  public Socket(Express app) {
     this.app = app;
     init();
   }
 
-  private void init(){
+  private static void broadcastMessage(String sender, Message msg) {
+    userUsernameMap.keySet().stream().filter(ctx -> ctx.session.isOpen()).forEach(session -> {
+      session.send(msg);
+    });
+  }
+
+  private void init() {
     app.ws("/chat", ws -> {
-      ws.onConnect(ctx -> System.out.println("Connected"));
+      ws.onConnect(ctx -> {
+        String username = "User" + nextUserNumber++;
+        userUsernameMap.put(ctx, username);
+        Message msg = new Message();
+        msg.setMsg(username + " joined the chat");
+        msg.setId(username);
+        broadcastMessage("Server", msg);
+      });
+
       ws.onMessage(ctx -> {
         Message msg = ctx.message(Message.class);
-        ctx.send(msg); // convert to json and send back
+        System.out.println(msg.getMsg());
+        broadcastMessage(userUsernameMap.get(ctx), msg);
       });
 
     });
