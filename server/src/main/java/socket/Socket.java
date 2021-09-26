7730ea2af59658ba2fd8841dd8dcd97acb3aa3cb
Alex <alexanderekdahl@me.com>
2021-09-26 15:29:44 +0200

    No needed to save in a hashmap

--- server/src/main/java/socket/Socket.java
+++ server/src/main/java/socket/Socket.java
@@ -2,12 +2,14 @@ package socket;
 
 import express.Express;
 import io.javalin.websocket.WsContext;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class Socket {
 
-  private static Map<WsContext, String> userUsernameMap = new ConcurrentHashMap<>();
+  private static final List<WsContext> userUsernameMap = new ArrayList<>();
   private static int nextUserNumber = 1;
 
   private final Express app;
@@ -17,28 +19,28 @@ public class Socket {
     init();
   }
 
-  private static void broadcastMessage(String sender, Message msg) {
-    userUsernameMap.keySet().stream().filter(ctx -> ctx.session.isOpen()).forEach(session -> {
-      session.send(msg);
+  private static void broadcastMessage(Message msg) {
+    userUsernameMap.stream().filter(ctx -> ctx.session.isOpen()).forEach(socket -> {
+      socket.send(msg);
     });
   }
 
   private void init() {
     app.ws("/chat", ws -> {
       ws.onConnect(ctx -> {
-        String username = "User" + nextUserNumber++;
-        userUsernameMap.put(ctx, username);
+        String id = "" + nextUserNumber++;
+        userUsernameMap.add(ctx);
         Message msg = new Message();
-        msg.setMsg(username + " joined the chat");
-        msg.setId(username);
+        msg.setMsg("A user joined the chat");
+        msg.setId(id);
         msg.setUuid("Server");
-        broadcastMessage("Server", msg);
+        broadcastMessage(msg);
       });
 
       ws.onMessage(ctx -> {
         Message msg = ctx.message(Message.class);
         System.out.println(msg.getMsg());
-        broadcastMessage(userUsernameMap.get(ctx), msg);
+        broadcastMessage(msg);
       });
 
     });
