cb6f2be47d9e8ae9ad16b9a4d605bdefd0441a45
Theodor-Bjornslatt <77094997+Theodor-Bjornslatt@users.noreply.github.com>
2021-09-28 21:48:22 +0200

    Added headlines for easier readability when scrolling data

--- client/src/components/BookingDetailCard.jsx
+++ client/src/components/BookingDetailCard.jsx
@@ -5,10 +5,10 @@ import HomeInfoCard from './HomeInfoCard'
 const BookingDetailCard = ({ id, home, startDate, endDate, totalPrice}) => {
   return (
     <div>
+      <h1>Booking</h1>
       <h3>Start date: {new Date(startDate).toLocaleDateString()}</h3>
       <h3>End date: {new Date(endDate).toLocaleDateString()}</h3>
       <h4>TotalPrice: {totalPrice}</h4>
-      <h3>Home</h3>
       <HomeInfoCard home={home} />
       <CreateReviewForm bookingDetailId={id} hostUserId={home.host.userID}/>
     </div>
