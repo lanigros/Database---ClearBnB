0da6976080d184b03c71298d4f80cd4b307fe52a
Alex <alexanderekdahl@me.com>
2021-09-29 14:09:45 +0200

    implemented methods to get and save msg

--- server/src/main/java/socket/Socket.java
+++ server/src/main/java/socket/Socket.java
@@ -5,21 +5,26 @@ import io.javalin.websocket.WsContext;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import model.Message;
+import service.SocketService;
 
 public class Socket {
 
-  private static final List<WsContext> userUsernameMap = new ArrayList<>();
-  private static int nextUserNumber = 1;
+  private final List<WsContext> userUsernameMap = new ArrayList<>();
+  private int nextUserNumber = 1;
+  private final SocketService socketService;
 
   private final Express app;
 
   public Socket(Express app) {
     this.app = app;
+    this.socketService = new SocketService();
     init();
   }
 
-  private static void broadcastMessage(Message msg) {
+  private void broadcastMessage(Message msg) {
     userUsernameMap.stream().filter(ctx -> ctx.session.isOpen()).forEach(socket -> {
       socket.send(msg);
     });
@@ -28,16 +33,23 @@ public class Socket {
   private void init() {
     app.ws("/chat", ws -> {
       ws.onConnect(ctx -> {
-        String id = "" + nextUserNumber++;
         userUsernameMap.add(ctx);
-        Message msg = new Message(id,"Server", "A user joined the chat");
+        Message msg = new Message("Server", "A user joined the chat");
         broadcastMessage(msg);
+        List<Message> list = socketService.getAll();
+        ctx.send(list);
       });
 
       ws.onMessage(ctx -> {
         Message msg = ctx.message(Message.class);
-        System.out.println(msg.getMsg());
-        broadcastMessage(msg);
+        Optional<Message> savedMsg = socketService.saveMessage(msg);
+
+        if(savedMsg.isPresent()){
+          broadcastMessage(savedMsg.get());
+        } else {
+          ctx.send(socketService.returnErrorMsg());
+        }
+
       });
 
     });
